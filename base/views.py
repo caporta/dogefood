584e6bb5c2780043062943cf61cab51b81b8bd15
Chris Aporta <christopher.aporta@gmail.com>
2016-07-25 01:11:33 -0400

    register Product with admin. create & render template for product_list @ products/

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'base/product_list.html', {'products': products})
