6fa75bb56d87461ecc3161ba91bff314eb8d3c06
Chris Aporta <christopher.aporta@gmail.com>
2016-07-25 03:06:30 -0400

    implement get_object_or_404 for product_detail view

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Product
 
@@ -8,5 +8,5 @@ def product_list(request):
     return render(request, 'base/product_list.html', {'products': products})
 
 def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
     return render(request, 'base/product_detail.html', {'product': product})
