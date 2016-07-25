99fb8d0f9210b24befd3ad360bbb6153471c081d
Chris Aporta <christopher.aporta@gmail.com>
2016-07-25 03:16:25 -0400

    use linebreaks template filter to dynamically create <p> tags for content

--- base/views.py
+++ base/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Product
 
+
 # Create your views here.
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'base/product_list.html', {'products': products})
+
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
