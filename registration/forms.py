1d1a2a30a30ec0a064247c3f146a34182340f438
Chris Aporta <christopher.aporta@gmail.com>
2016-07-27 22:22:29 -0400

    build choice fields where appropriate in forms.

--- registration/forms.py
+++ registration/forms.py
@@ -15,10 +15,16 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileForm(forms.ModelForm):
+    STATE_OPTS = [
+        ('New York', 'NY'),
+        ('Massachusetts', 'MA'),
+        ('California', 'CA'),
+    ]
+
     address1 = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     address2 = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     city = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    state = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    state = forms.ChoiceField(choices=STATE_OPTS, widget=forms.Select(attrs={'class': 'form-control'}))
     zip_code = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
@@ -27,10 +33,18 @@ class UserProfileForm(forms.ModelForm):
 
 
 class PetForm(forms.ModelForm):
+    PET_TYPE_OPTS = [
+        ('dog', 'Dog'),
+    ]
+    SEX_OPTS = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+
     name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    pet_type = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    pet_type = forms.ChoiceField(choices=PET_TYPE_OPTS, widget=forms.Select(attrs={'class': 'form-control'}))
     breed = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    sex= forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    sex= forms.ChoiceField(choices=SEX_OPTS, widget=forms.RadioSelect())
     age = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     weight = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 
