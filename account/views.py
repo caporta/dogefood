871597dd81db9f37f54c5c47abdbdeb35fb020f4
Chris Aporta <christopher.aporta@gmail.com>
2016-08-07 15:45:48 -0400

    successfully return json data via ajax POST in to update Pet instance.

--- account/views.py
+++ account/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import RequestContext
 from django.template.loader import render_to_string
@@ -25,8 +25,18 @@ def pet_form(request, pk):
     if request.method == 'POST':
         form = PetForm(request.POST or None, instance=pet)
         if form.is_valid():
+            resp_data = {}
             form.save()
-            return redirect('/')
+
+            resp_data['name'] = pet.name
+            resp_data['pet_type'] = pet.pet_type
+            resp_data['breed'] = pet.breed
+            resp_data['sex'] = pet.sex
+            resp_data['age'] = pet.age
+            resp_data['weight'] = pet.weight
+
+            return JsonResponse(resp_data)
+
         # html = render_to_string(template_name, {'form': form, 'petpk': pk}, context_instance=RequestContext(request))
         # return HttpResponse(html)
 
--- static/dogefood/js/main.js
+++ static/dogefood/js/main.js
@@ -14,7 +14,33 @@ window.Dogefood = {};
             type: 'GET',
 
             success: function (data) {
-                $('#js--pet-form-target').empty().append(data);
+                $('#js--pet-form-target').append(data);
+            },
+            error: function (xhr) {
+                console.log(xhr.status + ": " + xhr.responseText);
+            }
+        });
+    }
+
+    function updatePet(primaryKey) {
+        $.ajax({
+            url: 'pet/update/' + primaryKey,
+            type: 'POST',
+            data: {
+                'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
+                'name': $('#id_name').val(),
+                'pet_type': $('#id_pet_type').val(),
+                'breed': $('#id_breed').val(),
+                'sex': $('input[id^=id_sex_]:checked').val(),
+                'age': $('#id_age').val(),
+                'weight': $('#id_weight').val()
+            },
+
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (xhr) {
+                console.log(xhr.status + ": " + xhr.responseText);
             }
         });
     }
@@ -24,6 +50,14 @@ window.Dogefood = {};
         console.log(pk); //sanity check
         e.preventDefault();
         getPetForm(pk);
+    });
+
+
+    $('body').on('submit', '#pet-form', function (e) {
+        e.preventDefault();
+        console.log('form-submitted');
+        var pk = $(this).attr('action').split('/')[2];
+        updatePet(pk);
     });
 
     $('body').on('hidden.bs.modal', '.modal', function () {
