e4fab729879589634fc316e21dd1bfd73e02bff1
Chris Aporta <christopher.aporta@gmail.com>
2016-08-07 10:29:20 -0400

    render PetForm to home page via ajax GET request. WIP.

--- account/views.py
+++ account/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+
+from registration.models import Pet
+from registration.forms import PetForm
 
 
 @login_required(login_url='login/')
@@ -9,5 +14,18 @@ def home(request):
     return render(request, 'home.html', {'pets': pets})
 
 
-def pet_update(request, pk, template_name=''):
-    pass
+def pet_form(request, pk):
+    template_name = 'pet_form.html'
+    pet = get_object_or_404(Pet, pk=pk)
+    if request.method == 'GET':
+        form = PetForm(instance=pet)
+        html = render_to_string(template_name, {'form': form})
+        return HttpResponse(html)
+        #return render(request, template_name, {'form': form})
+
+
+
+
+
+
+
--- static/dogefood/js/main.js
+++ static/dogefood/js/main.js
@@ -6,6 +6,25 @@ window.Dogefood = {};
 (function() {
 
     Dogefood.constructors = {};
-    console.log("Hello World!");
+    console.log("Hello World!"); //sanity check
+
+    function getPetForm(primaryKey) {
+        $.ajax({
+            url: 'pet/update/' + primaryKey,
+            type: 'GET',
+
+            success: function (data) {
+                $('body').append(data);
+            }
+        });
+    }
+
+    $('div[id^=js--pet-info-panel-]').on('click', 'a[id^=js--get-pet-form-]', function (e) {
+        var pk = $(this).attr('id').split('-')[5];
+        console.log(pk); //sanity check
+        e.preventDefault();
+        getPetForm(pk);
+    });
+
 
 })();
