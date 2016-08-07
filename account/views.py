7d93f95002a2aa4842b1434fe7ba38c543006c21
Chris Aporta <christopher.aporta@gmail.com>
2016-08-07 12:25:20 -0400

    use RequestContext to pass csrf_token to template. expand CRUD for Pet instances with an Update action

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -19,9 +20,15 @@ def pet_form(request, pk):
     pet = get_object_or_404(Pet, pk=pk)
     if request.method == 'GET':
         form = PetForm(instance=pet)
-        html = render_to_string(template_name, {'form': form})
+        html = render_to_string(template_name, {'form': form, 'petpk': pk}, context_instance=RequestContext(request))
         return HttpResponse(html)
-        #return render(request, template_name, {'form': form})
+    if request.method == 'POST':
+        form = PetForm(request.POST or None, instance=pet)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        # html = render_to_string(template_name, {'form': form, 'petpk': pk}, context_instance=RequestContext(request))
+        # return HttpResponse(html)
 
 
 
